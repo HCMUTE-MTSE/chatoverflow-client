03e8e4995e7383e99ad3c5d213513f393b66369e
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2025-10-26 11:02:34 +0700

    adjust the collection's auth

--- app/routes/collections/my-collections.tsx
+++ app/routes/collections/my-collections.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 import MyQuestions from '~/components/page/collections/MyQuestions';
 import MyBlogs from '~/components/page/collections/MyBlogs';
+import { useNavigate } from 'react-router';
 
 function MyCollections() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  if (!token) {
+    navigate('/login');
+  }
+
   return (
     <div>
       <MyQuestions />
